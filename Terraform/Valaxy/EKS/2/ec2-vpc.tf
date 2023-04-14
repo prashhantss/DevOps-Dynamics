fbcf1417b923e8dbe3399805029c788afa540830
Prashant Patil <89540652+prashhantss@users.noreply.github.com>
2023-04-14 04:31:03 +0000

    Update

--- Terraform/Valaxy/EKS/2/ec2-vpc.tf
+++ Terraform/Valaxy/EKS/2/ec2-vpc.tf
@@ -108,5 +108,6 @@ module "sgs" {
 module "eks" {
   source = "./eks"
   sg_ids = module.sgs.security_group_public
+  vpc_id = aws_vpc.demo-vpc.id
   subnet_ids = [aws_subnet.demo_subnet-1.id, aws_subnet.demo_subnet-2.id]
 }
