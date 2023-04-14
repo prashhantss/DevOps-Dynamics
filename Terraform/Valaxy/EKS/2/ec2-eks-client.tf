afa8dec07fd4174a333a3d7ce9bc1dd8132559c2
Prashant Patil <89540652+prashhantss@users.noreply.github.com>
2023-04-14 05:26:30 +0000

    Update

--- Terraform/Valaxy/EKS/2/ec2-eks-client.tf
+++ Terraform/Valaxy/EKS/2/ec2-eks-client.tf
@@ -3,6 +3,6 @@ resource "aws_instance" "eks-client" {
  key_name = var.key 
  instance_type  = var.instance-type
  associate_public_ip_address = true
-subnet_id = aws_subnet.demo_subnet-1.id
+subnet_id = aws_subnet.eks-subnet-1.id
 vpc_security_group_ids = [aws_security_group.ec2-vpc-sg.id]
 }
